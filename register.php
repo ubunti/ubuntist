6844efece9daac6d56a5becbd5b0503a0ef7fd98
ubunti <jpchvltvb@meta.ua>
2019-07-15 09:16:04 +0300

    11

--- register.php
+++ register.php
@@ -4,9 +4,58 @@
     <meta charset="utf-8">
     <title> Как с помощью PHP и MySQL создать систему регистрации и авторизации пользователей</title>
     <link href="css/style.css" media="screen" rel="stylesheet">
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'rel='stylesheet' type='css'>
 </head>
 <body>
+<?php
+
+// Соединямся с БД
+$link=mysqli_connect("localhost", "mysql_user", "mysql_password", "base1");
+
+if(isset($_POST['submit']))
+{
+$err = [];
+
+// проверям логин
+if(!preg_match("/^[a-zA-Z0-9]+$/",$_POST['login']))
+{
+$err[] = "Логин может состоять только из букв английского алфавита и цифр";
+}
+
+if(strlen($_POST['login']) < 3 or strlen($_POST['login']) > 30)
+{
+$err[] = "Логин должен быть не меньше 3-х символов и не больше 30";
+}
+
+// проверяем, не сущестует ли пользователя с таким именем
+$query = mysqli_query($link, "SELECT user_id FROM users WHERE user_login='".mysqli_real_escape_string($link, $_POST['login'])."'");
+if(mysqli_num_rows($query) > 0)
+{
+$err[] = "Пользователь с таким логином уже существует в базе данных";
+}
+
+// Если нет ошибок, то добавляем в БД нового пользователя
+if(count($err) == 0)
+{
+
+$login = $_POST['login'];
+
+// Убераем лишние пробелы и делаем двойное хеширование
+$password = md5(md5(trim($_POST['password'])));
+
+mysqli_query($link,"INSERT INTO users SET user_login='".$login."', user_password='".$password."'");
+header("Location: login.php"); exit();
+}
+else
+{
+print "<b>При регистрации произошли следующие ошибки:</b><br>";
+foreach($err AS $error)
+{
+print $error."<br>";
+}
+}
+}
+?>
 <div class="container mregister">
     <div id="login">
         <h1>Регистрация</h1>
