2afee01aa936491598db517148f39e34a72deeea
ubunti <jpchvltvb@meta.ua>
2019-07-29 20:22:06 +0300

    200

--- register.php
+++ register.php
@@ -8,9 +8,40 @@
 </head>
 <body>
 <?php
+class User{
+
+   public $login, $password, $email, $created_at;
+
+    function __construct($login, $password, $email, $created_at)
+       {
+           $this->$login=$login;
+           $this->$password =$password;
+           $this->$email=$email;
+           $this->$created_at =$created_at;
+
+//далее проверка на валидность данных о юзере, и
+//если валидны отсылка к методу insertUser()
+//если нет, выдаем сообщение
+
+   function insertUser()
+        {
+$sql = "INSERT INTO users SET login = '$login', email = '$email', password = '$password', created_at = '$created_at';";
+        $link = mysqli_connect("localhost", "php", "Qwe123_!z", "base1");
+$query = mysqli_query($link, $sql);
+if ($query) {
+    echo 'ok';
+} else {
+    echo 'loose';
+    echo mysqli_error($link);
+}
+        }
+
+}
+
+
+
 // Добавлять в отчет все ошибки PHP (см. список изменений)
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+
 /*
 $login = $_POST['username'];
 // добавим в запрос остальные данные
@@ -18,7 +49,7 @@ $password = $_POST['password'];
 $email = $_POST['email'];
 $created_at = time();
 */
-class User
+/*class User
 {
     public $login;
     public $password;
@@ -40,6 +71,7 @@ class User
         $this->password = $password;
         $this->created_at = $created_at;
     }
+/*
 
     /*
   $user = new User('Login');
@@ -53,31 +85,24 @@ class User
   }
   error_reporting(E_ALL);
      */
-    public function info()
+    /*
+public function info()
     {
         return [$this->login];
     }
-$user new User('login');
+$user = new User('login');
 $res = $user->info();
 print_r($res);
 }
+*/
 
-$sql = "INSERT INTO users SET login = '$login', email = '$email', password = '$password', created_at = '$created_at';";
 
-$link = mysqli_connect("localhost", "php", "Qwe123_!z", "base1");
 
 // надо еще делать проверку на наличие ооединения, но пока не будем
 
-$query = mysqli_query($link, $sql);
 // сделаем проверку на успешное сохранение
 // вылезла ошибка, надо посмотреть какая
-if ($query) {
-    echo 'ok';
-} else {
-    echo 'loose';
-    echo mysqli_error($link);
-}
-var_dump($_POST);
+
 // чтобы лучше видно было, оожно добавлять
 // $_POST это масив, в еем хранятся данные их получаем оо ючччу.
 // посмотрели, что запрос сформировасся, скопировали, пихнули в бд
