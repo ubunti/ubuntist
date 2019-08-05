ecea6a13dff49ea209ae15238b203933f4044203
ubunti <jpchvltvb@meta.ua>
2019-08-05 15:25:02 +0300

    2001

--- Connnect.php
+++ Connnect.php
@@ -7,18 +7,12 @@ class Connnect
 
     public function __construct($user, $password, $database, $host = 'localhost')
     {
-        $this->$user = $user;
-        $this->$password = $password;
-        $this->$database = $database;
-
-    }
-
-
-    public function connect()
-    {
         $link = mysqli_connect($host, $user, $password, $database)
         or die("Ошибка " . mysqli_error($link));
         $this->link = $link;
+        $this->$user = $user;
+        $this->$password = $password;
+        $this->$database = $database;
 
     }
 
