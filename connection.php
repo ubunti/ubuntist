60cd1921ec3f0aa4a21f53f8a297291a06bd4016
ubunti <jpchvltvb@meta.ua>
2019-08-05 16:51:33 +0300

    jknbjk

--- connection.php
+++ connection.php
@@ -3,4 +3,4 @@ $host = 'localhost'; // адрес сервера
 $database = 'base2'; // имя базы данных
 $user = 'php'; // имя пользователя
 $password = 'Qwe123_!z'; // пароль
-?>
+require_once 'Connect.php';
