0cc8c32eb57c2d2b706fd76db1485cf465a3a08f
ubunti <jpchvltvb@meta.ua>
2019-08-07 21:51:59 +0300

    jknbjk

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    $h = date('H');
+    $img = (int)($h/6);
+    ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <style>
+        {
+        background: url(img/<?php echo $img ?>.img);
+        background-size: cover;
+        color: #ff0;
+        }
+    </style>
+
+</head>
+<body>
+<h1></h1><?php echo $h ?></h1>
+</body>
+</html>
