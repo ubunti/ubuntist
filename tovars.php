f12b8cbedf34109b0209d411ddfa05f8ae5d8e52
ubunti <jpchvltvb@meta.ua>
2019-08-15 09:37:52 +0300

    jknbjk

--- tovars.php
+++ tovars.php
@@ -7,18 +7,18 @@
 <?php
 require_once 'connection.php'; // подключаем скрипт
 $link = mysqli_connect($host, $user, $password, $database);
-
 if(isset($_POST['name']) && isset($_POST['company'])){
     // экранирования символов для mysql
     $name = htmlentities(mysqli_real_escape_string($link, $_POST['name']));
     $company = htmlentities(mysqli_real_escape_string($link, $_POST['company']));
-
     // создание строки запроса
     $query ="INSERT INTO tovars VALUES(NULL, '$name','$company')";
+    echo $query;
 
     // выполняемd запрос
     //$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
     $db->query($query);
+    $result = $db;
     if($result)
     {
         echo "<span style='color:blue;'>Данные добавлены</span>";
