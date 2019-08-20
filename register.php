48ffbb5560409a544c49e483a26d4d8123770d0b
ubunti <jpchvltvb@meta.ua>
2019-08-20 12:39:14 +0300

    jknbjk55

--- register.php
+++ register.php
@@ -1,11 +1,18 @@
 <html>
 <body>
+<?php
+$login = $_POST["login"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+$name = $_POST["name"];
 
+
+?>
 <form action="" method="post">
     Login: <input type="text" name="login"><br>
     E-mail: <input type="text" name="email"><br>
-    Пароль: <input type="password" name="password"><br>
     Имя: <input type="text" name="name"><br>
+    Пароль: <input type="password" name="password"><br>
     <p><input name="submit" type="submit" id="submit" value="Зарегистрироваться" /></form>
 
 </body>
