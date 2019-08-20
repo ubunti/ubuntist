fcde961e3983781410bd5171cbdeea8d300bcb3c
ubunti <jpchvltvb@meta.ua>
2019-08-20 12:07:01 +0300

    jknbjk55

--- register.php
+++ register.php
@@ -2,10 +2,10 @@
 <body>
 
 <form action="" method="post">
-    Login: <input type="text" name="name"><br>
+    Login: <input type="text" name="дщпшт"><br>
     E-mail: <input type="text" name="email"><br>
-    Пароль: <input type="password" name="email"><br>
-    Имя: <input type="text" name="email"><br>
+    Пароль: <input type="password" name="password"><br>
+    Имя: <input type="text" name="name"><br>
     <p><input name="submit" type="submit" id="submit" value="Зарегистрироваться" /></form>
 
 </body>
