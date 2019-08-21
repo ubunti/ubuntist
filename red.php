ee5593cd39a93134425a80934fce9050f5e95dca
ubunti <jpchvltvb@meta.ua>
2019-08-21 19:49:20 +0300

    jknbjk55

--- red.php
+++ red.php
@@ -13,7 +13,7 @@ or die("Ошибка " . mysqli_error($link));
 
 
 // если запрос POST
-if(isset($_POST['name']) && isset($_POST['login']) && isset($_POST['email']) && isset($_POST['password'])  && isset($_POST['id'])){
+if( isset($_POST['login']) && isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password'])  && isset($_POST['id'])){
 
     $id = htmlentities(mysqli_real_escape_string($link, $_POST['id']));
     $login = htmlentities(mysqli_real_escape_string($link, $_POST['login']));
@@ -32,7 +32,7 @@ if(isset($_POST['name']) && isset($_POST['login']) && isset($_POST['email']) && 
 if(isset($_GET['id']))
 
 {
-    //$id = (int)$_GET['id'];
+    $id = (int)$_GET['id'];
     $id = isset($_GET['id']) ? (int) $_GET['id'] : 0;
     // создание строки запроса
     $query ="SELECT * FROM users1 WHERE id = '$id'";
@@ -42,18 +42,17 @@ if(isset($_GET['id']))
     if($result && mysqli_num_rows($result)>0)
     {
         $row = mysqli_fetch_row($result); // получаем первую строку
-        $name = $row[1];
-        $company = $row[2];
+
 
         echo "<h2>Изменить данные</h2>
             <form method='POST'>
             <input type='hidden' name='id' value='$id' />
             <p>Введите логин:<br> 
-            <input type='text' name='name' value='$name' /></p>
-            <p>емеил: <br> 
             <input type='text' name='login' value='$login' /></p>
-            <p>Имя: <br> 
+           <p>емеил: <br> 
             <input type='text' name='email' value='$email' /></p>
+            <p>Имя: <br> 
+            <input type='text' name='name' value='$name' /></p>
             <p>пароль: <br> 
             <input type='text' name='password' value='$password' /></p>
             <input type='submit' value='Сохранить'>
